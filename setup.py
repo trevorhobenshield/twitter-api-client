76f7b8737d9a3c3f41a7f2d877ce6709f061e238
trevorhobenshield <trevorhobenshield@gmail.com>
2023-03-11 11:45:37 -0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="twitter-api-client",
-    version="0.0.5",
+    version="0.0.6",
     description="Twitter API",
     author="Trevor Hobenshield",
     author_email="trevorhobenshield@gmail.com",
