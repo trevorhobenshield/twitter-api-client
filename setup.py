f49990a0834eb5e71b0be9295a9ea7b0d65ef298
trevorhobenshield <trevorhobenshield@gmail.com>
2023-03-11 11:13:42 -0800

    update

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="twitter-api-client",
+    version="0.0.4",
+    description="Twitter API",
+    author="Trevor Hobenshield",
+    author_email="trevorhobenshield@gmail.com",
+    url="https://github.com/trevorhobenshield/twitter-api",
+    install_requires=[
+        "ujson",
+        "aiohttp",
+        "requests",
+    ],
+    keywords="twitter api client async search automation bot scrape",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'src': ['*']}
+)
