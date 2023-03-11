b5446fc32b769e49e06bc9b8effbc5b0b98987f4
trevorhobenshield <trevorhobenshield@gmail.com>
2023-03-11 12:21:13 -0800

    added twitter search

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="twitter-api-client",
-    version="0.0.6",
+    version="0.0.7",
     description="Twitter API",
     author="Trevor Hobenshield",
     author_email="trevorhobenshield@gmail.com",
