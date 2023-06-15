42b7dfb66add9d44f4ce57ddcf00098513beb4c4
trevorhobenshield <trevorhobenshield@gmail.com>
2023-06-15 11:23:34 -0700

    cleanup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
 
-#import shutil;import pathlib;[shutil.rmtree(p) for p in pathlib.Path().iterdir() if p.name in {'env', 'dist', 'twitter_api_client.egg-info'}];
 python3 -m build
 python3 -m twine upload dist/*
